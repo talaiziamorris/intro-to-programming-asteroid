04add9a378edb91a103d6c45607d3c80db249b9a
Talaizia Morris <talaiziamorris@talaizias-air.myfiosgateway.com>
2022-03-30 22:37:36 -0400

    added skills section to index.html

--- js/index.js
+++ js/index.js
@@ -1,8 +1,30 @@
 var today = new Date();
-var thisYear = Date().getFullYear(); 
+var thisYear = today.getFullYear(); 
+//console.log(today);//
+//console.log(thisYear);//
 
 const footer = document.querySelector('footer');
 const copyright = document.createElement('p');
 
 copyright.innerHTML = `Talaizia Morris ${thisYear}`;
 footer.append(copyright);
+
+const skills = [
+    'typing',
+    'microsoft applications',
+    'JavaScript',
+    'HTML'
+];
+
+const skillsSection = document.getElementById('skills');
+const skillsList = skillsSection.querySelector('ul');
+console.log(skillsSection);
+console.log(skillsList); 
+
+for (let listItem of skills) {
+    const skill = document.createElement('li');
+    skill.innerHTML = listItem;
+    skillsList.append(skill);
+    
+    //console.log(listItem);//
+};
